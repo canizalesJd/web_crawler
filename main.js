f85e9a9ea39fd815eb05f36d2a435df56eb19fae
canizalesJd <canizalesjd@gmail.com>
2024-08-19 20:46:12 -0600

    Adding crawlpage function

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('Hello World');
+import { crawlPage } from "./functions/crawl";
+
+async function main() {
+    if (process.argv.length < 3) {
+        console.log('no website provided');
+        return;
+    }
+    if (process.argv.length > 3) {
+        console.log('too many arguments provided');
+        return;
+    }
+    const baseURL = process.argv[2];
+
+    console.log(`starting crawl of: ${baseURL}...`);
+
+    await crawlPage(baseURL);
+}
+
+main();
