086447fa0246afd7628defce12e0db7f4c6daefa
canizalesJd <canizalesjd@gmail.com>
2024-08-19 20:16:40 -0600

    Adding getURLsFromHTML function

--- functions/crawl.js
+++ functions/crawl.js
@@ -1,3 +1,5 @@
+import { JSDOM } from 'jsdom';
+
 const normalizeURL = (url) => {
     const parsedUrl = new URL(url);
     const { host, pathname } = parsedUrl;
@@ -6,4 +8,24 @@ const normalizeURL = (url) => {
     return normalizedUrl;
 };
 
-export { normalizeURL };
+const getURLsFromHTML = (html, baseURL) => {
+    const urls = [];
+    const dom = new JSDOM(html);
+    const anchors = dom.window.document.querySelectorAll('a');
+
+    for (const anchor of anchors) {
+        if (anchor.hasAttribute('href')) {
+            let href = anchor.getAttribute('href');
+
+            try {
+                href = new URL(href, baseURL).href;
+                urls.push(href);
+            } catch (err) {
+                console.log(`${err.message}: ${href}`);
+            }
+        }
+    }
+    return urls;
+};
+
+export { normalizeURL, getURLsFromHTML };
